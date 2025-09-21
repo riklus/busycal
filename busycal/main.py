36ed4e16f0d849153ef387ae88a573d5c2c52adb
Riccardo Lussana <lussana.riccardo@gmail.com>
2025-09-21 18:30:39 +0200

    prepend text to event description

--- busycal/main.py
+++ busycal/main.py
@@ -8,6 +8,7 @@ from busycal import app
 
 BUSYCAL_TOKEN = os.getenv("BUSYCAL_TOKEN")
 RE_TRANSPARENT = re.compile(r"^\bTRANSP:TRANSPARENT\b", flags=re.MULTILINE)
+RE_DESCRIPTION = re.compile(r"^\bDESCRIPTION:\b", flags=re.MULTILINE)
 
 
 @app.route("/")
@@ -18,10 +19,13 @@ async def ical():
         abort(403)
 
     ical = request.args.get("ical")
+    prepend = request.args.get("prepend")
 
     async with httpx.AsyncClient() as client:
         res = await client.get(ical)
 
     busy = RE_TRANSPARENT.sub(r"TRANSP:OPAQUE", res.text)
+    if prepend:
+        busy = RE_TRANSPARENT.sub(rf"TDESCRIPTION:{prepend} ", res.text)
 
     return Response(busy, mimetype="text/plain")
